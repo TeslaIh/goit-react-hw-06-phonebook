065129c92ff1973bad6b3ef3d33aa042884d4e40
TeslaIh <87144117+TeslaIh@users.noreply.github.com>
2022-04-20 19:53:14 +0300

    first try
    change components style

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,16 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Label } from "./Filter.styled";
 
-const Filter = ({ handleChange, filter }) => (
-    <Label>
-        Find contacts by Name
-        <input onChange={handleChange} type="text" name="filter" value={filter} />
-    </Label>
+const Filter = ({ changeFilter, filter }) => (
+  <Label>
+    Find contacts by Name
+    <input onChange={changeFilter} type="text" name="filter" value={filter} />
+  </Label>
 );
 
 export default Filter;
 
 Filter.propTypes = {
-  handleChange: PropTypes.func.isRequired,
+  changeFilter: PropTypes.func.isRequired,
   filter: PropTypes.string.isRequired,
 };
---
+++ src/redux/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persisteContactReducer } from './contactsSlice';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+export const store = configureStore({
+  reducer: {
+    contacts: persisteContactReducer,
+    },
+
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
+});
+
+export const persistor = persistStore(store);
